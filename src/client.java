3b0bbae424757870b7a4f5bfd1f6f6f8910e5f7b
AkashicLog <37618691+AkashicLog@users.noreply.github.com>
2020-06-11 18:07:31 +0800

    deleted

--- src/client.java
+++ src/client.java
@@ -1,10 +1,5 @@
-import entity.*;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import java.security.Security;
-
-
 public class client {
     public static void main(String[] args){
-        Security.addProvider(new BouncyCastleProvider());
+        
     }
 }
